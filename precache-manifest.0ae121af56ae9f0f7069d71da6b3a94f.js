ea9bb712d112248758811e3f29b587aa295ab44e
ashawkey <963113090@qq.com>
2020-06-27 09:14:52 +0800

    Updates

---
+++ precache-manifest.0ae121af56ae9f0f7069d71da6b3a94f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f57cb07eab9ff928d44458e685787b4d",
+    "url": "/blogs/index.html"
+  },
+  {
+    "revision": "fe1951bdd67b04914864",
+    "url": "/blogs/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "901583ee10f65197a134",
+    "url": "/blogs/static/js/2.c24a221a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/blogs/static/js/2.c24a221a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe1951bdd67b04914864",
+    "url": "/blogs/static/js/main.d05ba802.chunk.js"
+  },
+  {
+    "revision": "646b980ab9a6c5d87a2c",
+    "url": "/blogs/static/js/runtime-main.3a112643.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/blogs/static/media/logo.5d5d9eef.svg"
+  }
+]);
